b6b4aa0b8affd4d20ea44d203b676db70edd1168
info.francescobianco@gmail.com <info.francescobianco@gmail.com@28822550-7e3d-c8ea-783f-b2256eb1cb9b>
2011-10-11 14:53:05 +0000

    solved name problem

--- src/org/javanile/me/chess/engines/Engine.java
+++ src/org/javanile/me/chess/engines/Engine.java
@@ -9,6 +9,19 @@ package org.javanile.me.chess.engines;
  * @author cicciodarkast
  */
 public class Engine {
+    private Node start_node;
+    private Node brain_node;
+    
+    public void doMove(Move m) {
+    }
+    public void unMove() {   
+    }
     
     
+    private void brain_doMove(Move m) {
+    
+    }
+    private void brain_unMove(Move m) {
+    
+    }
 }
